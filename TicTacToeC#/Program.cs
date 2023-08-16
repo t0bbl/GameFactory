bfbf480abf4131acdcdae414ee4b129317964e03
robin droste <117731660+t0bbl@users.noreply.github.com>
2023-08-16 13:18:26 +0200

    refactor, add class for gameboard, refactor for that

--- TicTacToeC#/Program.cs
+++ TicTacToeC#/Program.cs
@@ -1,31 +1,74 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace TicTacToe
 {
     class Program
     {
-        static int[] board = new int[9];
+        static GameBoard gameBoard = new GameBoard();
+        static string player1, player2;
+        static int scorePlayer1, scorePlayer2;
         static void Main(string[] args)
-        {
-            for (int i = 0; i < 9; i++)
-            {
-                board[i] = 0;
-            }
-            Console.WriteLine("Input name of first Challenger");
-            string player1 = Console.ReadLine();
-            Console.WriteLine("Input name of second Challenger");
-            string player2 = Console.ReadLine();
+        { 
 
+            Console.WriteLine("Input name of first Challenger");
+            player1 = Console.ReadLine();
+            Console.WriteLine("Input name of second Challenger");
+            player2 = Console.ReadLine();
+            GameBoard gameBoard = new GameBoard();
+
+            do
+            {
+                playGame();
+                gameBoard.ResetBoard();
+
+                Console.WriteLine("Score:  " + player1 + " : " + scorePlayer1 + " !     " + player2 + " : " + scorePlayer2 + "!");
+                Console.WriteLine("Want a rematch Noob ? Y / N ?");
+
+             } while (Console.ReadLine().ToLower() == "y") ;
+        }
+
+        private class  GameBoard
+        {
+            private int[] board = new int[9];
+            public GameBoard()
+            {
+                ResetBoard();
+            }
+
+            public void ResetBoard()
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    board[i] = 0;
+                }
+            }
+            public int GetCell(int index)
+            {
+                return board[index];
+            }
+
+            public void SetCell(int index, int value)
+            {
+                if (index >= 0 && index < 9)
+                {
+                    board[index] = value;
+                }
+            }
+        }
+        private static int playGame()
+        {
 
             int player1turn = -1;
             int player2turn = -1;
+
             Random rand = new Random();
 
             int startingPlayer = rand.Next(1, 3);
 
             Console.WriteLine(startingPlayer == 1 ? player1 + " starts!" : player2 + " starts!");
 
-          
+
 
             while (checkForWinner() == 0)
             {
@@ -33,15 +76,15 @@ namespace TicTacToe
                 {
                     Console.WriteLine(startingPlayer == 1 ? player1 + ", input a number from 0 to 8" : player2 + ", input a number from 0 to 8");
                 }
-          
+
 
                 int currentPlayer = startingPlayer == 1 ? 1 : 2;
 
                 int chosenCell = int.Parse(Console.ReadLine());
 
-                if (board[chosenCell] == 0)
+                if (gameBoard.GetCell(chosenCell) == 0)
                 {
-                    board[chosenCell] = currentPlayer;
+                    gameBoard.SetCell(chosenCell, currentPlayer);
                     startingPlayer = startingPlayer == 1 ? 2 : 1;
                 }
                 else
@@ -51,117 +94,104 @@ namespace TicTacToe
                 }
 
                 printBoard();
-            
-        
-        }
-        int winnerNumber = checkForWinner();
+
+
+            }
+            int winnerNumber = checkForWinner();
 
             if (winnerNumber == 1)
             {
-                Console.WriteLine(player1 + " won the game! " + player2 + " should feel ashamed! Want a rematch Noob?");
+                Console.WriteLine(player1 + " won the game! " + player2 + " should feel ashamed!");
+                scorePlayer1 += 1;
             }
             else if (winnerNumber == 2)
             {
-                Console.WriteLine(player2 + " won the game! " + player1 + " is a utterly bad player! Want to go at it again?");
+                Console.WriteLine(player2 + " won the game! " + player1 + " is a utterly bad player!");
+                scorePlayer2 += 1;
             }
-            else if (winnerNumber == -1) 
+            else if (winnerNumber == -1)
             {
                 Console.WriteLine("It's a draw, Idiots!");
             }
+            return winnerNumber;
 
 
         }
-
-
         private static int checkForWinner()
         {
             // row 1-3
-            if (board[0] == board[1] && board[0] == board[2])
+            if (gameBoard.GetCell(0) == gameBoard.GetCell(1) && gameBoard.GetCell(0) == gameBoard.GetCell(2))
             {
-             //   Console.WriteLine("exit1");
-                return board[0];
+                return gameBoard.GetCell(0);
             }
-            if (board[3] == board[4] && board[3] == board[5])
+            if (gameBoard.GetCell(3) == gameBoard.GetCell(4) && gameBoard.GetCell(3) == gameBoard.GetCell(5))
             {
-             //   Console.WriteLine("exit2");
-                return board[3];
+                return gameBoard.GetCell(3);
             }
-            if (board[6] == board[7] && board[6] == board[8])
+            if (gameBoard.GetCell(6) == gameBoard.GetCell(7) && gameBoard.GetCell(6) == gameBoard.GetCell(8))
             {
-             //   Console.WriteLine("exit3");
-                return board[6];
+                return gameBoard.GetCell(6);
             }
             // column 1-3
-            if (board[0] == board[3] && board[0] == board[6])
+            if (gameBoard.GetCell(0) == gameBoard.GetCell(3) && gameBoard.GetCell(0) == gameBoard.GetCell(6))
             {
-             //   Console.WriteLine("exit4");
-                return board[0];
+                return gameBoard.GetCell(0);
             }
-            if (board[1] == board[4] && board[4] == board[7])
+            if (gameBoard.GetCell(1) == gameBoard.GetCell(4) && gameBoard.GetCell(1) == gameBoard.GetCell(7))
             {
-             //   Console.WriteLine("exit5");
-                return board[1];
+                return gameBoard.GetCell(1);
             }
-            if (board[2] == board[5] && board[2] == board[8])
+            if (gameBoard.GetCell(2) == gameBoard.GetCell(5) && gameBoard.GetCell(2) == gameBoard.GetCell(8))
             {
-             //   Console.WriteLine("exit6");
-                return board[2];
+                return gameBoard.GetCell(2);
             }
             // diagonal
-            if (board[0] == board[4] && board[0] == board[8])
+            if (gameBoard.GetCell(0) == gameBoard.GetCell(4) && gameBoard.GetCell(0) == gameBoard.GetCell(8))
             {
-             //   Console.WriteLine("exit7");
-                return board[0];
+                return gameBoard.GetCell(0);
             }
-            if (board[2] == board[4] && board[4] == board[6])
+            if (gameBoard.GetCell(2) == gameBoard.GetCell(4) && gameBoard.GetCell(2) == gameBoard.GetCell(6))
             {
-             //   Console.WriteLine("exit8");
-                return board[2];
+                return gameBoard.GetCell(2);
             }
-            //// draw
-            //{
-            //    for (int i = 0; i < board.Length; i++)
-            //    {
-            //        if (board[i] == 0)
-            //        {
-            ////            Console.WriteLine("exitdraw");
 
-            //            return -1;
-            //        }
+            // Check for draw (Uncomment if needed)
+            //for (int i = 0; i < 9; i++)
+            //{
+            //    if (gameBoard.GetCell(i) == 0)
+            //    {
+            //        return -1;
             //    }
             //}
-            return 0;
 
+            return 0;
         }
+
         private static void printBoard()
         {
-
             for (int i = 0; i < 9; i++)
             {
-                // print the board
+                int cellValue = gameBoard.GetCell(i);
                 // print x or o
                 // 0 nothing, 1 is x, 2 is o
-                if (board[i] == 0)
+                switch (cellValue)
                 {
-                    Console.Write(".");
-                }
-                if (board[i] == 1)
-                {
-                    Console.Write("X");
-                }
-                if (board[i] == 2)
-                {
-                    Console.Write("O");
+                    case 0:
+                        Console.Write(".");
+                        break;
+                    case 1:
+                        Console.Write("X");
+                        break;
+                    case 2:
+                        Console.Write("O");
+                        break;
                 }
 
                 // lines
-
                 if (i == 2 || i == 5 || i == 8)
                 {
                     Console.WriteLine();
                 }
-
-
             }
         }
     }
