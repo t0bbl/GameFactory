c60f95297c50c1afdbb0bc0fcb817137ca739b80
Robin Droste <r.droste@zubIT.de>
2023-09-05 10:04:01 +0200

    delete comments

--- TicTacToeC#/Mechanics/PlayGame.cs
+++ TicTacToeC#/Mechanics/PlayGame.cs
@@ -19,26 +19,6 @@ namespace TicTacToe
             {
                 if (choosenGame == null)
                 {
-                    //Console.Write("Which Game shall we play? ");
-                    //for (int i = 0; i < validGames.Count; i++)
-                    //{
-                    //    Console.Write(validGames[i]);
-                    //    if (i < validGames.Count - 1)
-                    //    {
-                    //        Console.Write(", ");
-                    //    }
-                    //}
-                    //Console.WriteLine("?");
-                    //game = Console.ReadLine();
-                    //if (validGames.Contains(game))
-                    //{
-                    //    choosenGame = game;
-                    //}
-                    //else
-                    //{
-                    //    Console.WriteLine("Invalid game. Try again.");
-                    //    continue;
-                    //}
                     game = Menu.ShowMenu(validGames);
                     choosenGame = game;
                 }
