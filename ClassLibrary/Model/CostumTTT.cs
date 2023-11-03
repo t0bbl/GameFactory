a122f11e819d548641d7ba28fffa5d7288cf86b0
Robin Droste <r.droste@zubIT.de>
2023-11-03 09:27:58 +0100

    Add summaries for costumTTT

--- ClassLibrary/Model/CostumTTT.cs
+++ ClassLibrary/Model/CostumTTT.cs
@@ -9,6 +9,9 @@
         private Random p_random = new Random();
         #endregion
 
+        /// <summary>
+        /// Initializes a new instance of the CustomTTT class, allowing for a twist mode or a custom-defined game setup. The twist mode uses predefined board dimensions, whereas the custom setup prompts the user for dimensions. Game type is set based on the selected mode and the board is reset accordingly.
+        /// </summary>
         public CustomTTT(bool p_Twist)
             : base(
                   p_Twist ? 8 : AskForRows(),
@@ -20,6 +23,10 @@
             this.p_Twist = p_Twist;
             ResetBoard();
         }
+
+        /// <summary>
+        /// Implements the primary game mechanics for the current game session. It prompts players for their move, validates the input, and updates the game board. Players take turns inputting coordinates for their move. If the twist mode is active, specific game dynamics are applied after a valid move. Player moves and game states are saved for record-keeping.
+        /// </summary>
         public override void GameMechanic(List<Player> p_Player)
         {
             base.GameMechanic(p_Player);
@@ -74,6 +81,9 @@
         }
 
         #region CustomSetup
+        /// <summary>
+        /// Prompts the user to specify the number of rows for the game board. If the input is valid, it returns the specified number; otherwise, it defaults to 3 rows.
+        /// </summary>
         private static int AskForRows()
         {
             Console.Write("Enter the number of rows: ");
@@ -85,6 +95,9 @@
             Console.WriteLine("Invalid input for rows. Using default value of 3.");
             return 3;
         }
+        /// <summary>
+        /// Prompts the user to specify the number of columns for the game board. If the input is valid, it returns the specified number; otherwise, it defaults to 3 columns.
+        /// </summary>
         private static int AskForColumns()
         {
             Console.Write("Enter the number of columns: ");
@@ -96,6 +109,9 @@
             Console.WriteLine("Invalid input for columns. Using default value of 3.");
             return 3;
         }
+        /// <summary>
+        /// Prompts the user to specify the required consecutive markers for a win. If the input is valid, it returns the specified number; otherwise, it defaults to a winning length of 3.
+        /// </summary>
         private static int AskForWinningLength()
         {
             Console.Write("Enter the winning length: ");
@@ -107,6 +123,9 @@
             Console.WriteLine("Invalid input for winning length. Using default value of 3.");
             return 3;
         }
+        /// <summary>
+        /// Introduces a twist mechanism to the game by potentially reversing the contents of the chosen column. The decision to twist is made randomly. If a twist occurs, the function flips the contents of the selected column and returns true. Otherwise, it returns false.
+        /// </summary>
         public bool TwistColumn(int p_chosenColumn)
         {
             bool ShouldTwist = p_random.Next(0, 2) == 0;
